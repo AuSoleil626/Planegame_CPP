b91e9dce2dfe99b8da4ea9d49793c767d57db964
AuSoleil626 <2230004148@student.must.edu.mo>
2024-05-31 18:13:42 +0800

    创建主循环函数，定义游戏状态枚举变量

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,35 @@
 #include "Game.h"
 
+Game::Game():frameTime(1000/refreshRate),game_state_(GameState())
+{
+    game_state_= gameBegin;
+}
+
+Game::~Game()
+{
+}
+
 void Game::GameStart()
 {
+    while (true)
+    {
+        auto startTime = std::chrono::steady_clock::now();
+
+        //render thread
+        
+        //logic thread
+        
+        auto endTime = std::chrono::steady_clock::now();
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+        if (elapsedTime < frameTime)
+        {
+            std::this_thread::sleep_for(frameTime - elapsedTime);
+        }
+    }
     return;
+}
+
+void Game::SetCurrentUI(std::shared_ptr<UIBase> ui)
+{
+    currentUI=ui;
 }
--- Game.h
+++ Game.h
@@ -1,7 +1,33 @@
 #pragma once
+#include <memory>
+#include <iostream>
+#include <chrono>
+#include <thread>
+enum GameState
+{
+    gameBegin,
+    gameLoading,
+    gameRunning,
+    gameEnd
+};
+class UIBase;
 
 class Game
 {
 public:
+    explicit Game();
+    ~Game();
+private:
+    std::shared_ptr<UIBase> currentUI;
+
+    //Frame rate
+    int refreshRate=60;
+    std::chrono::milliseconds frameTime;
+
+    //Gamestate
+    GameState game_state_;
+    
+public:
     void GameStart();
+    void SetCurrentUI(std::shared_ptr<UIBase> ui);
 };
